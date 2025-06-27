87ad1446ab1a858399d866509ca4ed31578722da
dutcu <dru.ttc@gmail.com>
2025-06-27 21:55:27 +0200

    wip

--- run_benchmark_das.sh
+++ run_benchmark_das.sh
@@ -14,28 +14,7 @@ for n in "${ns[@]}"; do
         echo "Starting benchmark for n=$n, adversary_behavior=$behavior..."
 
 
-        # Update globals.py
-        cat > "$GLOBALS_FILE" <<EOF
-import math
-n = $n  # current n
-t = int(math.floor(n/3))
-if n%3 == 0:
-    t -= 1
-MAX_ALIVE_PROCESSES = n - t
-
-QUESTION_MARK = "?"
-WAITING_MESSAGE = "waiting"
-
-RANDOM_CHOICE = 1
-INVALID_CHOICE = 2
-
-
-adversary_behavior = $behavior
-
-qb_per_process = 3  # usually calculated based on n
-EOF
-
-        prun -np 1 bash -c "python3 -u benchmark.py >${n}_${behavior}.log 2>&1" &
+        prun -np 1 bash -c "python3 -u benchmark.py --n ${n} --adv ${behavior} >${n}_${behavior}.log 2>&1" &
 
 
         # Save PID
